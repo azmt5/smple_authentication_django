cf574d14e1899a5e571b67924922c775f10c0edb
Azmat <ranaazmat89117@gmail.com>
2024-04-28 23:48:27 +0500

    Update views.py

--- views.py
+++ views.py
@@ -4,30 +4,11 @@
 from django.shortcuts import render ,HttpResponse ,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
-# Create your views here.
-# this url dispatch
-# in reder we pass two parameter 1.request and 2. page ka name
-# jub bi yaha sa dta khi bajna ho tu assa bajana or usy{{}} is use karna hy
-# data={
-#     'title':'created',
-#     'p':'welcome azmat back the millionare',
-#      'list':['java','script','python'],
-#       'dic':[
-#           {'name':'azmat','phone':342148041274},
-#           {'name':'ali','phone':3272812456426},
-#           {'name':'ahmad','phone':3272812456426},
-#           {'name':'nasir','phone':3272812456426}
-#       ]      
-# }
+
 
 
 def homepage(request):
-    return render(request,'index.html')  # agar template file ko excess karna ho tu render use ho ga
-
-
-# def aboutdetails(request,aboutid):
-#         return render(request,'index.html') 
-    # return HttpResponse(aboutid)
+    return render(request,'index.html') 
 
 def login(request):
      if request.method=='POST':
@@ -35,7 +16,7 @@ def login(request):
        passw=request.POST.get('password')
        user=authenticate(request,username=usern,password=passw)
        if user is not None:
-        #    login(request,user)
+  
            return redirect(homepage)
        else:
            return HttpResponse('not correct') 
@@ -52,9 +33,7 @@ def signup(request):
        my_user=User.objects.create_user(username,password1,password2)
        my_user.save()
        return redirect(login)
-    #    if password1!=password2:
-    #    else:
-    # my_user=user.objects.create_user(email,password1,password2)
+  
 
                            
     return render(request,'signup.html')
@@ -69,13 +48,4 @@ def logoutpage(request):
 
 
 
-# def about(request):
-#     return render(request,'about.html')
-# def services(request): 
-#     return render(request,'services.html')
 
-# def contact(request):
-#     return render(request,'contact.html')
-
-def contactDetails(request,contactid):      # this for daynamic urls 
-    return HttpResponse(contactid)
